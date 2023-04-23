3400965e54e0ddfa3f9eb41223c44e122de7561b
Johnsondoe119 <johnsondoe119@gmail.com>
2023-04-23 18:19:11 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,59 @@
+// Very Easy
+let john = 1999;
+let jay = 1968;
+let results = john - jay;
+
+console.log("The difference between John and Jay is", results);
+
+
+// Easy
+let name1 = "Didier Drogba";
+let name2 = "Thiery Henry";
+let numDif = name1.length - name2.length;
+
+console.log("Didier Drogba name is longer than Thiery Herny by", numDif, "letter." );
+
+// Medium
+// let userAnswer = prompt("Whats Up?");
+
+// if (userAnswer === userAnswer.toUpperCase()) {
+//     console.log("Why are you yelling?");
+// }   else if (userAnswer === userAnswer.toLowerCase()){
+//     console.log("What you say?");
+// }   else {
+//     console.log("Huh?");
+// }
+
+// Hard
+let x = 20;
+let y = 100;
+let resul1 = x + y;
+let result2 = y - x;
+let result3 = x * y;
+let result4 = y / x;
+
+console.log(resul1, result2, result3, result4);
+
+// Very Hard
+// alert("I can solve anything!!!")
+// let num1 = parseInt(prompt("Pick a Number"));
+// let asmd = prompt("+ - * or /");
+// let num2 = parseInt(prompt("Another Number"));
+
+// let x1 = num1;
+// let y1 = num2;
+
+// let add = x1 + y1;
+// let subtract = x1 - y1;
+// let multiply = x1 * y1;
+// let devide = x1 / y1;
+
+// if (asmd === "+"){
+//     document.write(num1 + "" + asmd + "" + num2 + "=" + add);
+// } else if (asmd === "-"){
+//     document.write(num1 + "" + asmd + "" + num2 + "=" + subtract);
+// }else if (asmd === "*"){
+//     document.write(num1 + "" + asmd + "" + num2 + "=" + multiply);
+// }else if (asmd === "/"){
+//     document.write(num1 + "" + asmd + "" + num2 + "=" + devide);
+// }
